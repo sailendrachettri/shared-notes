7e468e127e7911b857445f2695f11b5ebe69fea3
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-02-14 01:07:52 +0530

    loader added

--- shared-notes-software/src/areas/public/components/Sidebar.jsx
+++ shared-notes-software/src/areas/public/components/Sidebar.jsx
@@ -2,20 +2,27 @@ import React, { useEffect, useState } from "react";
 import { axiosInstance } from "../../../api/axios";
 import { GET_MST_NOTE_URL } from "../../../api/api_routes";
 
-
-
-const Sidebar = ({ setSelectedNoteId, sidebarItems, setSidebarItems, refresh }) => {
+const Sidebar = ({
+  setSelectedNoteId,
+  sidebarItems,
+  setSidebarItems,
+  refresh,
+}) => {
   const [active, setActive] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   const handleFetchAllItemList = async () => {
-   try {
-     //
-     const res = await axiosInstance.get(GET_MST_NOTE_URL);
-     console.log(res);
-     setSidebarItems(res?.data?.data || [])
-   } catch (error) {
+    try {
+      const res = await axiosInstance.get(GET_MST_NOTE_URL);
+      
+      setSidebarItems(res?.data?.data || []);
+    } catch (error) {
       console.log("not able to fetch sidebar items");
-   }
+    }finally{
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    }
   };
 
   useEffect(() => {
@@ -31,23 +38,29 @@ const Sidebar = ({ setSelectedNoteId, sidebarItems, setSidebarItems, refresh }) 
   return (
     <aside className="h-full flex flex-col bg-white ">
       {/* Project List */}
-      <div className="flex-1 overflow-y-auto px-4 space-y-1 custom-scrollbar">
-        {sidebarItems?.map((item) => (
-          <button
-            key={item?.note_id}
-            onClick={() => handleSelectNote(item?.note_id)}
-            className={`w-full text-sm text-left px-3 py-2 cursor-pointer rounded-xl transition-all duration-200
+      {loading ? (
+       <div className="w-full h-full flex items-center justify-center">
+         <div class="loader"></div>
+       </div>
+      ) : (
+        <div className="flex-1 overflow-y-auto px-4 space-y-1 custom-scrollbar">
+          {sidebarItems?.map((item) => (
+            <button
+              key={item?.note_id}
+              onClick={() => handleSelectNote(item?.note_id)}
+              className={`w-full text-sm text-left px-3 py-2 cursor-pointer rounded-xl transition-all duration-200
               ${
                 active === item?.note_id
                   ? "bg-slate-50 text-primary"
                   : "text-gray-600  hover:text-gray-500"
               }
             `}
-          >
-            {item?.note_title || ''}
-          </button>
-        ))}
-      </div>
+            >
+              {item?.note_title || ""}
+            </button>
+          ))}
+        </div>
+      )}
     </aside>
   );
 };
