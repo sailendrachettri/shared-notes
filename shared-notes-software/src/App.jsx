5108a0aed27046c3cb7e6c539ebe9831c190acf5
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-02-15 14:39:42 +0530

    custonm navbar added with logo

--- shared-notes-software/src/App.jsx
+++ shared-notes-software/src/App.jsx
@@ -1,15 +1,15 @@
-
-import './App.css'
-import Home from './areas/public/Home'
-
+import "./App.css";
+import Navbar from "./areas/public/common/nav-bar/Navbar";
+import Home from "./areas/public/Home";
 
 function App() {
-
   return (
     <>
-     <Home />
+      <Navbar />
+
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ shared-notes-software/src/areas/public/common/nav-bar/Navbar.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { getCurrentWindow } from "@tauri-apps/api/window";
+import logo from "../../../../assets/pngs/logo.png";
+import {
+  VscChromeMinimize,
+  VscChromeMaximize,
+  VscChromeRestore,
+  VscChromeClose,
+} from "react-icons/vsc";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const Navbar = () => {
+  const appWindow = getCurrentWindow();
+  const [isMaximized, setIsMaximized] = useState(false);
+  console.log(isMaximized);
+
+  useEffect(() => {
+    const checkMaximized = async () => {
+      const maximized = await appWindow.isMaximized();
+      console.log(maximized)
+      setIsMaximized(maximized);
+    };
+
+    checkMaximized();
+
+    const unlisten = appWindow.onResized(checkMaximized);
+
+    return () => {
+      unlisten.then((f) => f());
+    };
+  }, []);
+
+  const minimize = () => appWindow.minimize();
+
+  const maximize = async () => {
+
+    await appWindow.toggleMaximize();
+    const maximized = await appWindow.isMaximized();
+    console.log(maximized)
+    setIsMaximized(maximized);
+  };
+
+  const close = () => appWindow.close();
+  return (
+    <>
+      <div
+        data-tauri-drag-region
+        className="h-10 flex items-center justify-between px-4 bg-[#e5e7ed] text-slate-950"
+      >
+        {/* Left side - Logo */}
+        <div className="flex items-center gap-2">
+          <img src={logo} className="h-6 w-auto " />
+          <span className="text-sm font-medium ">SharedNotes</span>
+        </div>
+
+        {/* Right side - Window Controls */}
+        <div className="flex h-full">
+          <button
+            data-tauri-drag-region={false}
+            onClick={minimize}
+            className="w-12 flex items-center justify-center hover:bg-zinc-300"
+          >
+            <VscChromeMinimize size={14} />
+          </button>
+
+          <button
+            data-tauri-drag-region={false}
+            onClick={maximize}
+            className="w-12 flex items-center justify-center hover:bg-zinc-300"
+          >
+            {isMaximized ? (
+              <VscChromeRestore size={14} />
+            ) : (
+              <VscChromeMaximize size={14} />
+            )}
+          </button>
+
+          <button
+            data-tauri-drag-region={false}
+            onClick={close}
+            className="w-12 flex items-center justify-center hover:bg-red-600 hover:text-white"
+          >
+            <VscChromeClose size={14} />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
