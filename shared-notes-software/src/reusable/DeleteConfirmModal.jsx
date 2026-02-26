d176c53b81f402a611b12346399afb3b49248636
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-02-26 22:31:02 +0530

    ui ux fixes

--- shared-notes-software/src/reusable/DeleteConfirmModal.jsx
+++ shared-notes-software/src/reusable/DeleteConfirmModal.jsx
@@ -16,7 +16,7 @@ const DeleteConfirmModal = ({
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className="fixed inset-0 bg-black/50 backdrop-blur-sm z-40"
+            className="fixed inset-0 bg-black/50  z-40"
             onClick={onClose}
           />
 
